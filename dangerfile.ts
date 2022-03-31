5424c7c67e2577c3b09df8aa497644925da38416
Ostap Taran <me@austinate.me>
2022-03-31 22:57:19 +0300

    Add "danger-plugin-lint-report integration

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,21 @@
 import { message, danger, warn } from 'danger';
+import { schedule } from 'danger';
+
+// Disable ESLint warning import is not working here
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const reporter = require('danger-plugin-lint-report');
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+// Report ESLint result
+schedule(
+  reporter.scan({
+    fileMask: '**/reports/lint-results.xml',
+    reportSeverity: true,
+    requireLineModification: true,
+  }),
+);
 
 const app = danger.git.fileMatch('src/**/*.ts');
 const tests = danger.git.fileMatch('src/**/*.spec.ts');
