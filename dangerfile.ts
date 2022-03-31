dc70cc643308fea222c97c75ad8d593b0839eeeb
Ostap Taran <me@austinate.me>
2022-03-31 20:37:20 +0300

    Update dangerfile.ts
    Add test changes validation

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,11 @@
-import { message, danger } from 'danger';
+import { message, danger, warn } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+const app = danger.git.fileMatch('src/**/*.ts');
+const tests = danger.git.fileMatch('src/**/*.spec.ts');
+
+if (app.modified && !tests.modified) {
+  warn('You have app changes without tests.');
+}
