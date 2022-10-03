26b4d6603bce8f6e18baef15d0a0d0110b80d602
Ostap Taran <me@austinate.me>
2022-10-03 14:00:23 +0200

    Update `app.controller` tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { v4 as uuid } from 'uuid';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -21,7 +22,25 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {});
+  describe('auth', () => {
+    it('login request should return JWT if credentials are correct', async () => {
+      const request = {
+        user: { username: 'austinate', userId: uuid() },
+      };
+
+      const expectedToken = 'token';
+      authServiceMock.login.mockImplementationOnce((user) => {
+        expect(user.username).toBe(request.user.username);
+        expect(user.userId).toBe(request.user.userId);
+
+        return Promise.resolve({ accessToken: expectedToken });
+      });
+
+      expect(await appController.login(request)).toMatchObject({
+        accessToken: expectedToken,
+      });
+      expect(authServiceMock.login).toBeCalledTimes(1);
+      expect(authServiceMock.login).toBeCalledWith({ ...request.user });
+    });
   });
 });
